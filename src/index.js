233618887653f8c1a028816ed87a41b3257e0882
Max <likha.max@gmail.com>
2023-01-24 16:07:59 +0300

    fix: delete unnecessary variable

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 module.exports = function check(str, bracketsConfig) {
   if (str.length % 2 != 0) return false;
-  let count = [];
   for (let a = 0; a < str.length; a++) {
     bracketsConfig.forEach((i) => {
       if (!str.includes(i[0] || !str.includes(i[1]))) return false;
       if (str.includes(i.join(''))) {
-        count.push(str[a], str[a + 1]);
         str = str.replace(i.join(''), '');
         a = 0;
       }
