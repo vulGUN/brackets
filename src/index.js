0a87aa1daf4267e3e4925a364856809773264b91
Max <likha.max@gmail.com>
2023-01-24 16:55:44 +0300

    fix: delete unnecessary code line

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ module.exports = function check(str, bracketsConfig) {
   if (str.length % 2 != 0) return false;
   for (let a = 0; a < str.length; a++) {
     bracketsConfig.forEach((i) => {
-      if (!str.includes(i[0] || !str.includes(i[1]))) return false;
       if (str.includes(i.join(''))) {
         str = str.replace(i.join(''), '');
         a = 0;
       }
     });
   }
-  if (str.length != 0) return false;
+  if (str.length) return false;
   else return true;
 };
